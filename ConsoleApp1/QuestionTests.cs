fc2b81ae6e883fa045c839badedba41897bee0c2
Mustafagood <mustafa.swedish@gmail.com>
2025-02-26 21:19:09 +0100

    kommentarerna och förklaringen questiontests.cs

--- ConsoleApp1/QuestionTests.cs
+++ ConsoleApp1/QuestionTests.cs
@@ -6,7 +6,9 @@ public class QuestionTests
     [Fact]
     public void TestCorrectAnswer()
     {
+        // Skapar en ny fråga med två alternativ, där "Rätt" är det korrekta svaret.
         var question = new Question("Testfråga", new[] { "Fel", "Rätt" }, "Rätt");
+        // Kontrollerar att det korrekta svaret finns på rätt plats i listan.
         Assert.Equal(2, Array.IndexOf(question.Options, question.CorrectAnswer) + 1);
     }
 }
